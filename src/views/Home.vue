9bdc6c08243238fd8b348bce127339a0556a204d
Ed Corcoran <ed@mentallyfriendly.com>
2019-05-25 20:04:36 +1000

    basic landing page with address and contact

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="home">
-    Nowendoc Run
+    <div>
+      <img class="logo" src="../assets/nr-logo.svg"/>
+      <div class="info">
+        <p>6138 Brackendale Road
+        Nowendoc, NSW ‎2354
+        Australia</p>
+        <a href="mailto:contact@nowendoc.run?subject=Website Enquiry">contact@nowendoc.run</a>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.home {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  width: 20%;
+  min-width: 200px;
+}
+</style>
