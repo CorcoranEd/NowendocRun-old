2203daad0e938c7aa7c9874b917f8915020e8f71
Ed Corcoran <ed@mentallyfriendly.com>
2019-05-26 11:33:04 +1000

    favicon + lint

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Work+Sans:400,500,600,700&display=swap');
+@import url("https://fonts.googleapis.com/css?family=Work+Sans:400,500,600,700&display=swap");
 
 #app {
   font-family: "Work Sans", Helvetica, Arial, sans-serif;
