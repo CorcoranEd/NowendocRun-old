bf83bd8851ea6258b9b16e7ed1cb9fdc66161430
Ed Corcoran <ed@mentallyfriendly.com>
2019-05-26 16:49:46 +1000

    photo + contact + logo

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,73 @@
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css?family=Work+Sans:400,500,600,700&display=swap");
 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: "Work Sans", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #303030;
 }
 
 a {
   color: #303030;
+  margin-bottom: 0.5rem;
+}
+
+h1 {
+  font-family: "Work Sans", Helvetica, Arial, sans-serif;
+  font-weight: 500;
+  margin-bottom: 0.5em;
+}
+
+.wrapper {
+  margin: 0 auto;
+  width: 100%;
+  max-width: 900px;
+  padding: 0 2rem;
+}
+
+.column {
+  display: flex;
+  flex-flow: column nowrap;
+
+  &.half {
+    width: 50%;
+    padding: 0 1rem;
+  }
+}
+
+.row {
+  display: flex;
+  flex-flow: row nowrap;
+  margin: 0 -1rem;
+}
+
+.flex {
+  display: flex;
+  flex-flow: row nowrap;
+}
+
+@media only screen and (max-width: 1000px) {
+  .wrapper {
+    padding: 0 1rem;
+  }
+
+  .column {
+    &.half {
+      width: 100%;
+      padding: 0 0.5rem;
+    }
+  }
+
+  .row {
+    margin: 0 -0.5rem;
+    flex-flow: column nowrap;
+  }
 }
 </style>
