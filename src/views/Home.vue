2b62fbe1f14e658db2e659bf6e08e36b8da4ee75
Ed Corcoran <ed@mentallyfriendly.com>
2019-05-26 15:07:03 +1000

    photo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div>
+    <div class="tile">
       <img class="logo" src="../assets/nr-logo.svg" />
       <div class="info">
         <p>6138 Brackendale Road Nowendoc, NSW ‎2354 Australia</p>
@@ -23,10 +23,21 @@
   flex-flow: row nowrap;
   align-items: center;
   justify-content: center;
+  background-image: url("../assets/nowendoc-run.jpg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
 }
 
 .logo {
   width: 20%;
   min-width: 200px;
+  margin-bottom: 1rem;
+}
+
+.tile {
+  padding: 2rem 0;
+  background: rgba(255, 255, 255, 0.85);
 }
 </style>
