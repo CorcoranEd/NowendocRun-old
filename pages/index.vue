7c02b31b972aee7e8de235de5c9c28a28a0a91d7
Ed Corcoran <ed@mentallyfriendly.com>
2019-08-06 17:06:24 +1000

    converted project to nuxt

---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container mx-auto relative p-4">
+    <div class="absolute -z-10 w-full top-1/2 left-1/4">
+      <img class="opacity-5 w-full" src="../assets/logo-outline.svg">
+    </div>
+    <div>
+      <img src="../assets/nowendoc-run.jpg">
+    </div>
+    <div class="flex flex-row py-4">
+      <div class="w-3/4">
+        <div class="flex flex-col">
+          <div class="flex flex-col mb-2">
+            <h1>Nowendoc Run</h1>
+            <a
+              href="https://www.google.com/maps/place/6138+Brackendale+Rd,+Nowendoc+NSW+2354/@-31.4841013,151.677614,13.6z/data=!4m5!3m4!1s0x6b756a651cdac741:0x34a4e244f9a4de93!8m2!3d-31.4827523!4d151.7160276"
+              target="_blank"
+            >6138 Brackendale Road<br>
+              Nowendoc, NSW ‎2354<br>
+              Australia
+            </a>
+          </div>
+          <div class="flex flex-col mb-2">
+            <p>General Contact</p>
+            <a href="mailto:contact@nowendoc.run?subject=Website Enquiry">contact@nowendoc.run</a>
+          </div>
+          <div class="flex flex-col mb-2">
+            <p>Ross Henderson, Farm Manager</p>
+            <a href="mailto:ross@nowendoc.run">ross@nowendoc.run</a>
+            <a href="tel:‭0447 335 364‬">0447 335 364‬</a>
+          </div>
+          <div class="flex flex-col">
+            <p>Linda Wild, Farm Administrator</p>
+            <a href="mailto:linda@nowendoc.run">linda@nowendoc.run</a>
+            <a href="tel:‭0400 815 800‬‬">0400 815 800‬</a>
+          </div>
+        </div>
+      </div>
+      <div class="w-1/4">
+        <img src="../assets/logo-outline.svg">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
