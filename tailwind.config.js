7c85b119a16bde16f53bd4b4ce2724ac08fd2fb1
Ed Corcoran <ed@mentallyfriendly.com>
2019-08-12 14:48:52 +1000

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,22 @@ module.exports = {
       display: ['Work Sans', 'sans-serif'],
       body: ['Work Sans', 'sans-serif']
     },
-    zIndex: {
-      '-10': '-10'
-    },
-    inset: {
-      '1/2': '50%',
-      '1/4': '25%'
-    },
-    opacity: {
-      '5': '.05'
+    extend: {
+      zIndex: {
+        '-10': '-10'
+      },
+      inset: {
+        '-1/2': '-50%',
+        '-1/4': '-25%',
+        '1/2': '50%',
+        '1/4': '25%'
+      },
+      opacity: {
+        '5': '.05'
+      },
+      spacing: {
+        '128': '32rem'
+      }
     }
   },
   variants: {},
